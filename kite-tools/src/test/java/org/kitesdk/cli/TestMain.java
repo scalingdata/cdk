ce4cd27dea6b45b42a58c2774c75d52c5bb87db7
Joey Echeverria <joey@cloudera.com>
2014-08-14 11:52:16 -0700

    CDK-526: Added --version option to the Main CLI
    Closes #178.
    
    Conflicts:
    	kite-tools/src/test/java/org/kitesdk/cli/TestMain.java

--- kite-tools/src/test/java/org/kitesdk/cli/TestMain.java
+++ kite-tools/src/test/java/org/kitesdk/cli/TestMain.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.util.List;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 
@@ -110,8 +109,8 @@ public class TestMain {
         contains("Usage: {} [options] [command] [command options]"),
         eq(Main.PROGRAM_NAME));
     verify(console).info(contains("Options"));
-    verify(console).info(anyString(), anyString(), anyString(), anyString(),
-        anyString()); // -v
+    verify(console, times(2)).info(anyString(), anyString(),
+        anyString(), anyString(), anyString()); // -v, --version
     verify(console).info(contains("Commands"));
     verify(console).info(anyString(), eq("create"), anyString());
     verify(console).info(anyString(), eq("delete"), anyString());
@@ -156,6 +155,13 @@ public class TestMain {
     verify(console).info(anyString(), contains("this is a comment"));
     verify(console).info(anyString(),
         eq(Main.PROGRAM_NAME), eq("test"), eq("test dataset-name"));
+    assertEquals(0, rc);
+  }
+
+  @Test
+  public void testVersion() throws Exception {
+    int rc = run("--version");
+    verify(console).info(contains("kite version"), anyString());
     assertEquals(0, rc);
   }
 
