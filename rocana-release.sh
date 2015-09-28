760a1154d9a0c0ef3c8863662b3b645fcb425bfe
Joey Echeverria <joey42@gmail.com>
2015-09-28 08:37:01 -0400

    ROCANA-3400: Add HDP profile to Kite release job
    * Move deploy url/repository id to properties.
    * Fixed typo

--- rocana-release.sh
+++ rocana-release.sh
@@ -14,11 +14,21 @@ if [ -n "${WORKSPACE}" ]; then
   MVN_FLAGS="${MVN_FLAGS} -Dmaven.repo.local=${WORKSPACE}/.repository"
 fi
 
+error() {
+  local msg="$1"
+  local retcode="$2"
+
+  echo "Error: $msg"
+
+  [ -n "$retcode" ] && exit "$retcode"
+}
+
 # Set the version to the next release
 ${MVN} ${MVN_FLAGS} \
   versions:set \
     -DnewVersion=${KITE_RELEASE_VERSION} \
-    -DgenerateBackupPoms=false
+    -DgenerateBackupPoms=false ||
+error "Unable to update the release version to ${KITE_RELEASE_VERSION}" 1
 
 # Commit and push the version number update
 ${MVN} ${MVN_FLAGS} \
@@ -26,19 +36,34 @@ ${MVN} ${MVN_FLAGS} \
     -Dincludes=**/pom.xml \
     -Dexcludes=**/target/**/pom.xml \
   scm:checkin \
-    -Dmessage="ROCANA-BUILD: Preparing for release ${KITE_RELEASE_VERSION}"
+    -Dmessage="ROCANA-BUILD: Preparing for release ${KITE_RELEASE_VERSION}" ||
+error "Unable to checkin the update of the release version to ${KITE_RELEASE_VERSION}" 1
 
-# Deploy the release to the maven repo and tag the release in git
+# Deploy the release to the maven repo
 ${MVN} ${MVN_FLAGS} \
-  deploy \
+  clean \
+  deploy ||
+error "Unable to deploy CDH artifacts to nexus" 1
+
+# Deploy the hdp release to the maven repo
+${MVN} ${MVN_FLAGS} \
+  -Phdp2,hdp2-release \
+  clean \
+  deploy ||
+error "Unable to deploy HDP artifacts to nexus" 1
+
+# Tag the release in git
+${MVN} ${MVN_FLAGS} \
   scm:tag \
-    -Dtag=release-${KITE_RELEASE_VERSION}
+    -Dtag=release-${KITE_RELEASE_VERSION} ||
+error "Unable to create release tag" 1
 
 # Set the version to the next development version
 ${MVN} ${MVN_FLAGS} \
   versions:set \
     -DnewVersion=${KITE_DEVELOPMENT_VERSION} \
-    -DgenerateBackupPoms=false
+    -DgenerateBackupPoms=false ||
+error "Unable to update the development version to ${KITE_DEVELOPMENT_VERSION}" 1
 
 # Commit and push the version number update
 ${MVN} ${MVN_FLAGS} \
@@ -46,4 +71,5 @@ ${MVN} ${MVN_FLAGS} \
     -Dincludes=**/pom.xml \
     -Dexcludes=**/target/**/pom.xml \
   scm:checkin \
-    -Dmessage="ROCANA-BUILD: Preparing for ${KITE_DEVELOPMENT_VERSION} development"
+    -Dmessage="ROCANA-BUILD: Preparing for ${KITE_DEVELOPMENT_VERSION} development" ||
+error "Unable to checkin the update of the development version to ${KITE_DEVELOPMENT_VERSION}" 1
